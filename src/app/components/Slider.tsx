ddea0c221e4f4a557c4ce086e8819bc16ccc8524
Johnny Tai <bm414148@gmail.com>
2023-07-17 21:38:09 +0800

    make getItemsToShow() more readable

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -16,20 +16,25 @@ const SliderComponent = () => {
   const [itemsToShow, setItemsToShow] = useState(4)
   const [itemsToScroll, setItemsToScroll] = useState(window.innerWidth >= 1200 ? 4 : 1)
   useEffect(() => {
+
     const getItemsToShow = () => {
-      const { itemsToShow } = breakpoints.find(({ width }) => window.innerWidth >= width) || {};
-      return itemsToShow || 1;
+      const breakpoint = breakpoints.find(({ width }) => window.innerWidth >= width) || null; //if winnerWidth <640, itemsToShow will become undefined
+      return breakpoint ? breakpoint.itemsToShow : 1;
     };
+
     const handleResize = () => {
       setItemsToShow(getItemsToShow());
       setItemsToScroll(window.innerWidth >= 1200 ? 4 : 1)
     };
+
     handleResize(); // 初始化設定 itemsToShow 的值
     window.addEventListener('resize', handleResize);  //視窗大小變更時執行handleResize，使不用reload就可以改變itemToShow的value
+
     return () => {  //clean up func, prevent resource lick 
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
   const handleNext = () => {
     sliderRef.current!.slickNext(); //使用！ 確保 ref 不為 null
   };
