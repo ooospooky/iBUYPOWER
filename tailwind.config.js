916194a4d3481b1a475f697282f4214ae11ecd2e
Johnny Tai <bm414148@gmail.com>
2023-07-17 17:20:59 +0800

    ad 1199px breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,9 @@ module.exports = {
       screens: {
         "2xl": { max: "1535px" },
         // => @media (max-width: 1535px) { ... }
-
         xl: { max: "1279px" },
         // => @media (max-width: 1279px) { ... }
-
+        llg: { max: "1199px" },
         lg: { max: "1023px" },
         // => @media (max-width: 1023px) { ... }
 
