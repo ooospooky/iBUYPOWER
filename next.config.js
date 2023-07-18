c5a4dcbbbb980bba1b169a9f8c01d3052a6862cb
Johnny Tai <bm414148@gmail.com>
2023-07-18 13:58:14 +0800

    add images.domains tring to dispaly image on vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["https://i-buypower.vercel.app/"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
