537c5c824fab3f26b3fed52e1b576b7a3ab30e17
Johnny Tai <bm414148@gmail.com>
2023-07-18 13:00:46 +0800

    clean useless comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-// // import { Inter } from 'next/font/google'
-// import { Gotham } from '../font'
-// // const inter = inter({ subsets: ['latin'] })  
-// const gotham = Gotham({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
   title: 'iBUYPOWER',
   description: 'PCs and PC Builder ',
