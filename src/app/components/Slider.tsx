ca10aa0d349c4adca9bc11227fa39b80400bc4cc
Johnny Tai <bm414148@gmail.com>
2023-07-18 12:59:49 +0800

    make prev&next btn better

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -53,9 +53,9 @@ const SliderComponent = () => {
   return (
     <div id="container" className="">
       {/* only when itemToShow ===4,  display button   */}
-      <div className={`flex justify-end mr-4 -mb-16 z-10 relative ${itemsToShow !== 4 && 'hidden'} `} >
-        <button onClick={handlePrev} className='text-4xl shadow-md border-2 px-2 py-1 '>&lt;</button>
-        <button onClick={handleNext} className='text-4xl shadow-md border-2 px-2 py-1'>&gt;</button>
+      <div className={`flex justify-end gap-2 mr-4 -mb-16  z-10 relative  ${itemsToShow !== 4 && 'hidden'} `} >
+        <button onClick={handlePrev} className='text-4xl shadow-md border-2 px-3 py-1 hover:bg-[#3A3A3A] hover:text-white '>&lt;</button>
+        <button onClick={handleNext} className='text-4xl shadow-md border-2 px-3 py-1 hover:bg-[#3A3A3A] hover:text-white '>&gt;</button>
       </div>
       <Slider {...settings} ref={sliderRef}>
         {products.map((product) => (
