8db040d2ababffdeb15d526f619b5737b9d1106d
Johnny Tai <bm414148@gmail.com>
2023-07-18 14:14:11 +0800

    change back to webp

--- src/app/components/products.ts
+++ src/app/components/products.ts
@@ -4,7 +4,7 @@ export const products: IProduct[] = [
   {
     id: 1,
     type: "prebuilt",
-    img: "/pc/jpgCreatorRDYY40BG201.jpg",
+    img: "/pc/CreatorRDYY40BG201.webp",
     title: "Creator RDY Y40BG201",
     description: [
       "Windows 11 Pro",
